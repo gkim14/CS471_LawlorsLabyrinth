29efcdcbd5b097afbe02e4fcef0fd2c7ba1c9127
Grace Kim <gracekim@Graces-MacBook-Air-2.local>
2025-09-24 14:39:05 -0800

    wasd kinda works, no collision yet

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -1,1 +1,20 @@
 extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
+
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("left"):
+		direction = -1
+	if Input.is_action_pressed("right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
