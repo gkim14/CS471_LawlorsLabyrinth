6186ab0eb1fdcd0f556537e97552855ba4e6be61
Grace Kim <gkim14@alaska.edu>
2025-10-04 16:14:20 -0800

    player rotates to input direction

--- scripts/PlayerScript.gd
+++ scripts/PlayerScript.gd
@@ -1,28 +1,14 @@
 extends CharacterBody2D
 
 var speed = 400
-
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+var rotation_speed = 10
 
 func _physics_process(_delta):
-	get_input()
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	var target_angle = Vector2.UP.angle_to(velocity)
+	
+	if velocity.length() > 0:
+		rotation = lerp_angle(rotation, target_angle, _delta*rotation_speed)
+		
 	move_and_slide()
-#var angular_speed = PI
-#
-#
-#func _process(delta):
-	#var direction = 0
-	#if Input.is_action_pressed("left"):
-		#direction = -1
-	#if Input.is_action_pressed("right"):
-		#direction = 1
-#
-	#rotation += angular_speed * direction * delta
-#
-	#var velocity = Vector2.ZERO
-	#if Input.is_action_pressed("up"):
-		#velocity = Vector2.UP.rotated(rotation) * speed
-#
-	#position += velocity * delta
