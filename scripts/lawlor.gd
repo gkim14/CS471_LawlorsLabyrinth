be168123718f24a1007bd2d101fdc094cb27902c
Andrew Baluyot <127162735+ando-balu@users.noreply.github.com>
2025-10-11 21:52:54 -0800

    chore: cleanup file

--- scripts/lawlor.gd
+++ scripts/lawlor.gd
@@ -23,7 +23,6 @@ func _physics_process(_delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		Global.game_over = true
 		_handle_game_over()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
